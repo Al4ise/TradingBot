eda68b2128c0c1d769d959b335af33981265165b
Martin Pelteshki <39273158+Al4ise@users.noreply.github.com>
2024-06-04 00:19:12 +0300

    hy

--- main_finrl.py
+++ main_finrl.py
@@ -19,6 +19,15 @@ from finrl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
 from finrl import config_tickers
 from finrl.config import INDICATORS
 
+from finrl.meta.env_stock_trading.env_stocktrading import StockTradingEnv
+from finrl.agents.stablebaselines3.models import DRLAgent
+from stable_baselines3.common.logger import configure
+from finrl.main import check_and_make_directories
+from finrl.config import INDICATORS, TRAINED_MODEL_DIR, RESULTS_DIR
+
+check_and_make_directories([TRAINED_MODEL_DIR])
+
+
 from finta import TA
 
 import numpy as np
@@ -39,22 +48,24 @@ def main():
         'AAPL',
         'NVDA',
         'MSFT',
-        'SMCI',
-        'ARM'
+        'SMCI'
+        # 'ARM'
     ]
-    train, test = buildStockData(symbols)
-    # Save train and test data to different CSV files
-    train.to_csv('data/train.csv', index=False)
-    test.to_csv('data/test.csv', index=False)
 
-    #print(df.dtypes)
-    #print(df.head(10))
-    
-    #randomSteps(df)
+    # Save train and test data to different CSV files
+    train_path, test_path = buildStockData(symbols)
+
+    train = pd.read_csv(train_path)
+    train = train.set_index(train.columns[0])
+    train.index.names = ['']
+
+    test = pd.read_csv(test_path)
+    test = test.set_index(test.columns[0])
+    test.index.names = ['']
 
     # train model
     start_time = time.time()
-    trainModel(name, df, 100000)
+    trainModel(train)
     end_time = time.time()
 
     execution_time = end_time - start_time
@@ -63,96 +74,110 @@ def main():
     # eval
     evaluateModel(name, df)
 
-def randomSteps(df):
-    env = gym.make('stocks-v0', df=df, frame_bound=(5, 100), window_size=5)
-    #env.unwrapped.signal_features
+def makeGym(train):
+    stock_dimension = len(train.tic.unique())
+    state_space = 1 + 2*stock_dimension + len(INDICATORS)*stock_dimension
+    print(f"Stock Dimension: {stock_dimension}, State Space: {state_space}")
+    
+    buy_cost_list = sell_cost_list = [0.001] * stock_dimension
+    num_stock_shares = [0] * stock_dimension
 
-    state = env.reset()
-    # Test env, random steps
-    while True:
-        action = env.action_space.sample()
-        n_state, reward, terminated, truncated, info = env.step(action)
-        if terminated or truncated:
-            print("Info: ", info)
-            break
+    env_kwargs = {
+        "hmax": 100,
+        "initial_amount": 1000000,
+        "num_stock_shares": num_stock_shares,
+        "buy_cost_pct": buy_cost_list,
+        "sell_cost_pct": sell_cost_list,
+        "state_space": state_space,
+        "stock_dim": stock_dimension,
+        "tech_indicator_list": INDICATORS,
+        "action_space": stock_dimension,
+        "reward_scaling": 1e-4
+    }
 
-    plt.figure(figsize=(15,6))
-    plt.cla()
-    env.render()
-    plt.show()
+    e_train_gym = StockTradingEnv(df=train, **env_kwargs)
+    env_train, _ = e_train_gym.get_sb_env()
+    return env_train
 
 def buildStockData(symbols):
-    TRAIN_START_DATE = '2009-01-01'
-    TRAIN_END_DATE = '2023-05-01'
+    if not os.path.exists('data'):
+        os.makedirs('data')
+        
+    train_path = 'data/train.csv'
+    test_path = 'data/test.csv'
 
-    TEST_START_DATE = '2023-01-01'
-    TEST_END_DATE = '2024-05-01'
+    if not os.path.exists(train_path) or not os.path.exists(test_path):
+        TRAIN_START_DATE = '2009-01-01'
+        TRAIN_END_DATE = '2023-05-01'
 
-    df_raw = YahooDownloader(start_date=TRAIN_START_DATE, end_date=TRAIN_END_DATE, ticker_list=symbols).fetch_data()
+        TEST_START_DATE = '2023-05-02'
+        TEST_END_DATE = '2024-05-01'
 
-    fe = FeatureEngineer(use_technical_indicator=True, tech_indicator_list=INDICATORS,
-                         use_turbulence=True,
-                         use_vix=True,
-                         user_defined_feature=False)
+        df_raw = YahooDownloader(start_date=TRAIN_START_DATE, end_date=TEST_END_DATE, ticker_list=symbols).fetch_data()
+
+        fe = FeatureEngineer(use_technical_indicator=True, tech_indicator_list=INDICATORS,
+                            use_turbulence=True,
+                            use_vix=True,
+                            user_defined_feature=False)
 
 
-    processed = fe.preprocess_data(df_raw)
+        processed = fe.preprocess_data(df_raw)
 
-    list_ticker = processed["tic"].unique().tolist()
-    list_date = list(pd.date_range(processed['date'].min(),processed['date'].max().astype(str)))
-    combination = list(itertools.product(list_date,list_ticker))
+        list_ticker = processed["tic"].unique().tolist()
+        list_date = list(pd.date_range(processed['date'].min(),processed['date'].max()).astype(str))
+        combination = list(itertools.product(list_date,list_ticker))
 
-    processed_full = pd.DataFrame(combination,columns=["date", "tic"]).merge(processed,on=["date","tic"],how="left")
-    processed_full = processed_full[processed_full['date'].isin(processed['date'])]
-    processed_full = processed_full.sort_values(['date','tic'])
-    processed_full = processed_full.fillna(0)
+        processed_full = pd.DataFrame(combination,columns=["date","tic"]).merge(processed,on=["date","tic"],how="left")
+        processed_full = processed_full[processed_full['date'].isin(processed['date'])]
+        processed_full = processed_full.sort_values(['date','tic'])
 
-    train = data_split(processed_full, TRAIN_START_DATE, TRAIN_END_DATE)
-    test = data_split(processed_full, TEST_START_DATE, TEST_END_DATE)
+        processed_full = processed_full.fillna(0)
 
-    return train, test
+        train = data_split(processed_full, TRAIN_START_DATE, TRAIN_END_DATE)
+        test = data_split(processed_full, TEST_START_DATE, TEST_END_DATE)
 
-def formatStockData(csvpath):
-    df = pd.read_csv(csvpath)
-    df['Date'] = pd.to_datetime(df['Date'])
-    df.set_index('Date', inplace=True)
-    df.sort_values('Date', ascending=True, inplace=True)
-    df['Volume'] = df['Volume'].astype(float)
-    return df
+        train.to_csv(train_path, index=False)
+        test.to_csv(test_path, index=False)
 
-def applyTechnicals(df):
-    #df['SMA'] = TA.SMA(df, 7)
-    #df['MACD'] = TA.MACD(df)
-    df['RSI'] = TA.RSI(df)
-    #df['RSI'] = TA.
+    return train_path, test_path
 
-    #df['MACD_signal'], df['MACD_histogram'] = TA.MACD(df)
-    #df['MACD_signal'] = df['MACD_signal'].astype(float)
-    #df['MACD_histogram'] = df['MACD_histogram'].astype(float)
 
-    #df['VWAP'] = TA.VWAP(df)
+def trainModel(train):
+    env_train = makeGym(train)
 
-    df.fillna(0, inplace=True)
+    agent = DRLAgent(env = env_train)
 
-    return df
+    # Set the corresponding values to 'True' for the algorithms that you want to use
+    if_using_a2c = True
+    if_using_ddpg = False
+    if_using_ppo = False
+    if_using_td3 = False
+    if_using_sac = False
 
-def add_signals(env):
-    start = env.frame_bound[0] - env.window_size
-    end = env.frame_bound[1]
-    prices = env.df.loc[:, 'Low'].to_numpy()[start:end]
-    signal_features = env.df.loc[:, ['Low', 'High', 'Volume', 'Open', 'Close', 'RSI']].to_numpy()[start:end]
-    return prices, signal_features
+    model_a2c = agent.get_model("a2c")
+    model_ppo = agent.get_model('ppo')
 
-def trainModel(ticker, df, timesteps):
-    env_maker = lambda: customEnv(df=df, frame_bound=(5, 10000), window_size=5)
-    env = make_vec_env(env_maker, vec_env_cls=SubprocVecEnv, n_envs=20)
+    if if_using_a2c:
+        # set up logger
+        tmp_path = RESULTS_DIR + '/a2c'
+        new_logger_a2c = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+        # Set new logger
+        model_a2c.set_logger(new_logger_a2c)
 
-    #model = A2C('MlpPolicy', env, device='cuda', verbose=1)
-    model = A2C('MlpPolicy', env, device='cpu', verbose=1)
-    #model = A2C('MlpPolicy', env, device='cpu', verbose=1, policy_kwargs=dict(optimizer_class=RMSpropTFLike, optimizer_kwargs=dict(eps=1e-5)))
+    if if_using_ppo:
+        # set up logger
+        tmp_path = RESULTS_DIR + '/ppo'
+        new_logger_ppo = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+        # Set new logger
+        model_ppo.set_logger(new_logger_ppo)
 
-    model.learn(total_timesteps=timesteps)
-    model.save(f'models/{ticker}')
+    trained_a2c = agent.train_model(model=model_a2c,tb_log_name='a2c',total_timesteps=50000) if if_using_a2c else None
+
+    trained_ppo = agent.train_model(model=model_ppo,tb_log_name='ppo',total_timesteps=50000) if if_using_ppo else None
+       
+    trained_a2c.save(TRAINED_MODEL_DIR + "/agent_a2c") if if_using_a2c else None # type: ignore
+    trained_ppo.save(TRAINED_MODEL_DIR + "/agent_ppo") if if_using_ppo else None # type: ignore
+       
 
 def evaluateModel(ticker, df):
     env_maker = lambda: customEnv(df=df, frame_bound=(10000, 11000), window_size=5, render_mode='human')
@@ -172,9 +197,6 @@ def evaluateModel(ticker, df):
     plt.cla()
     env.render()
     plt.show()
-
-class customEnv(StocksEnv):
-    _process_data = add_signals
 
 if __name__ == '__main__':
     main()
